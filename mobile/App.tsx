3fde38d5aa6b0f7939d9854aae3187f620b619c1
Andre Alves <andresistinfo@gmail.com>
2023-01-20 20:40:58 +0000

    Installed talwind and nativiwind and created the components for frontend

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,8 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import './src/lib/dayjs'
+import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter'
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -13,22 +15,9 @@ export default function App() {
     );
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>NLW App!</Text>
+    <>
+      <Home />
       <StatusBar barStyle="light-content" backgroundColor="purple" translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#FFF',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
